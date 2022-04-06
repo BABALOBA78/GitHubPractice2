4f6cc0e6f60f182923809adb210e06f25693fc71
Joel John <jjohn3593@stu.d214.org>
2022-04-06 13:09:10 -0500

    cell tableview delegate code

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    
+    
+    @IBOutlet weak var  tableView: UITableView!
+    var animalNames = ["turtle", "toad", "lizard"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tablecell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        tablecell.textLabel?.text = animalNames[indexPath.row]
+        return tablecell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animalNames.count
+    }
 
 }
 
