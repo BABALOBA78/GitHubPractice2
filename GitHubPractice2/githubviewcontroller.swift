3c375ed231de379c54da78d1f0a4d499a4fa4386
nicholas deaconu <nicholasdeaconu5072@stu.d214.org>
2022-04-06 10:46:38 -0700

    sequel added

---
+++ GitHubPractice2/githubviewcontroller.swift
@@ -0,0 +1,29 @@
+//
+//  githubviewcontroller.swift
+//  GitHubPractice2
+//
+//  Created by Nick Deaconu on 4/6/22.
+//
+
+import UIKit
+
+class githubviewcontroller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
